7113410cddc190d572857362265d8e8f7b4dce46
Malalatiana Sarah Ravelonarivo <mravelon@c1r6s10.42antananarivo.mg>
2025-01-29 15:33:48 +0300

    add param exec_pl

--- projection/ray.c
+++ projection/ray.c
@@ -6,7 +6,7 @@
 /*   By: aelison <aelison@student.42antananarivo.m  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 13:50:33 by aelison           #+#    #+#             */
-/*   Updated: 2025/01/29 14:44:06 by mravelon         ###   ########.fr       */
+/*   Updated: 2025/01/29 15:32:30 by mravelon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ void	exec(t_scene *scene, t_maps *ptr, t_vect wind)
 	if (ptr->type == Sphere)
 		exec_sp(scene, ptr->struct_obj, &r, wind);
 	if (ptr->type == Plane)
-		exec_pl(scene, ptr->struct_obj, &r, wind);
+		exec_pl(scene, ptr->struct_obj, &r, wind.x, wind.y, get_type(scene->world, Sphere));
 	if (ptr->type == Cylinder)
 		exec_cy(scene, ptr->struct_obj, &r, wind);
 }
