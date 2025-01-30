9218ae0a1ea075afc540010d4e39706b37d7915e
aelison <aelison@student.42antananarivo.mg>
2025-01-30 08:08:04 +0300

    error message must be followed by newline ?
    Read subject

--- errors/check_file.c
+++ errors/check_file.c
@@ -68,7 +68,7 @@ static int	create_object(char *tmp, int line, int *res)
 	valid_ok = get_required_obj(val);
 	if (val == 0)
 	{
-		ft_putstr_fd("Error: wrong format on line ", STDERR_FILENO);
+		ft_putstr_fd("Error:\nWrong format on line ", STDERR_FILENO);
 		ft_putnbr_fd(line, STDERR_FILENO);
 		ft_putchar_fd('\n', STDERR_FILENO);
 		*res = EXIT_FAILURE;
@@ -117,13 +117,11 @@ int	check_file(char *str)
 	fd = open(str, O_RDONLY);
 	if (fd < 0)
 	{
-		ft_putstr_fd("Error: file: can't open ", STDERR_FILENO);
+		ft_putstr_fd("Error:\nFile: can't open ", STDERR_FILENO);
 		ft_putendl_fd(str, STDERR_FILENO);
 		return (EXIT_FAILURE);
 	}
 	result = file_content(fd);
-	if (result == EXIT_FAILURE)
-		ft_putstr_fd("Error: parse: failed to create obj\n", STDERR_FILENO);
 	close(fd);
 	return (result);
 }
