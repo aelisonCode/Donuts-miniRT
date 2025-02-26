ed8b49611aa02cc6bbc78fd06a8f6565805e67f1
aelison <aelison@student.42antananarivo.mg>
2025-02-26 15:08:04 +0300

    color upgrade

--- color/create.c
+++ color/create.c
@@ -10,8 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../header/color.h"
-#include "../header/struct.h"
+#include "../header/mini_rt.h"
 
 void	ft_scale_color(int *color_base, int keycode, int incr)
 {
@@ -22,7 +21,33 @@ void	ft_scale_color(int *color_base, int keycode, int incr)
 	else if (keycode == SCALE_DOWN && *color_base > 0)
 		*color_base -= incr;
 }
+/*================== Start V2 =======================*/
+/*V2 diffuse_reflection : Lambertienne*/
+/*To apply to each object*/
+double	brightness_reflection(t_l light, t_vect v_normal, t_vect point)
+{
+	t_vect v_light;
+	double	tmp;
 
+	v_light = ft_normalize(substraction(light.pos, point));
+
+	tmp = fmin(scalaire(v_light, v_normal), 0);
+	return (light.bright *  COEFF_REFCT * tmp);
+}
+
+/*V2 apply brightness_reflection to each color*/
+double diff_reflect(double bright, int color)
+{
+	return (bright * (color / 255.0));
+}
+
+/*V2 ambient color*/
+double	am_reflect(double am_ratio, int color)
+{
+	return (am_ratio * (color / 255.0) * REFRACTION_AM);
+}
+
+/* ================ End V2 ================== */
 double	create_ambient(t_a *ambient, double color)
 {
 	double	res;
